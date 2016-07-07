decab14f037260d9759f25f5a2e593106c2754a3
Ilya Storozhilov <itorozhilov@dnanexus.com>
2016-07-07 19:06:57 +0300

    PTFM-14073 APIserver mock prototype

--- src/python/test/test_dxpy_api_mock.py
+++ src/python/test/test_dxpy_api_mock.py
@@ -17,7 +17,11 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-import os, unittest
+import os
+import unittest
+import tempfile
+import subprocess
+import time
 
 #from __future__ import print_function, unicode_literals, division, absolute_import
 
@@ -37,15 +41,38 @@ import os, unittest
 #from dxpy.utils.resolver import resolve_path, resolve_existing_path, ResolutionError, is_project_explicit
 
 class TestDxpy(unittest.TestCase):
+    apiserverMockHandlerFilename = None
+
     def setUp(self):
-        # TODO: Start APIserver mock-object
+        # Preparing APIserver mock object HTTP-handler code
+        fd, self.apiserverMockHandlerFilename = tempfile.mkstemp(suffix=".py", prefix="apiserver_mock_handler_definition.")
+        fh = os.fdopen(fd, 'w')
+        fh.write("""\
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+class MockHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write("Hello World !")
         return
+""")
+        fh.close()
+        # Starting APIserver mock-object
+        apiServerMockFilename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "apiserver_mock.py")
+        self.apiServerMockSubprocess = subprocess.Popen([apiServerMockFilename, self.apiserverMockHandlerFilename])
+        time.sleep(1)
 
     def tearDown(self):
-        # TODO: Stop APIserver mock-object
+        # Stopping APIserver mock-object
+        self.apiServerMockSubprocess.kill()
+        # Removing mock object HTTP-handler code
+        os.remove(self.apiserverMockHandlerFilename)
         return
 
     def test_exponential_retry(self):
+        time.sleep(1)
         self.assertTrue(True)
 
 if __name__ == '__main__':
