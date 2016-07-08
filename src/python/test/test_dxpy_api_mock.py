ef8937a834aa35ae6ef81c038e83efa7732ee928
Ilya Storozhilov <itorozhilov@dnanexus.com>
2016-07-08 18:49:59 +0300

    PTFM-14073 APIserver mock emulates 503

--- src/python/test/test_dxpy_api_mock.py
+++ src/python/test/test_dxpy_api_mock.py
@@ -22,44 +22,33 @@ import unittest
 import tempfile
 import subprocess
 import time
-import requests
 import json
-
-#from __future__ import print_function, unicode_literals, division, absolute_import
-
-#import os, unittest, tempfile, filecmp, time, json, sys
-#import shutil
-#import string
-#import subprocess
-#import platform
-
-#import requests
-#from requests.packages.urllib3.exceptions import SSLError
-
-#import dxpy
-#import dxpy_testutil as testutil
-#from dxpy.exceptions import (DXAPIError, DXFileError, DXError, DXJobFailureError, ResourceNotFound)
-#from dxpy.utils import pretty_print, warn
-#from dxpy.utils.resolver import resolve_path, resolve_existing_path, ResolutionError, is_project_explicit
+import dxpy
+import requests
 
 class TestDxpyApiMock(unittest.TestCase):
     apiServerMockSubprocess = None
 
     def setUp(self):
+        # Setting up DXPY to use APIserver mock object
+        apiServerTcpPort = 8080
+        dxpy.set_api_server_info(host="127.0.0.1", port=apiServerTcpPort, protocol="http")
+
         # Starting APIserver mock-object
         apiServerMockFilename = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                "apiserver_mock", "apiserver_mock.py")
+                "mock_api", "apiserver_mock.py")
         apiServerMockHandlerFilename = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                "apiserver_mock", "test_503_no-retry-after_exponential_randomized_timout.py")
-        self.apiServerMockSubprocess = subprocess.Popen([apiServerMockFilename, apiServerMockHandlerFilename])
+                "mock_api", "test_503_no-retry-after_exponential_randomized_timout.py")
+        self.apiServerMockSubprocess = subprocess.Popen([apiServerMockFilename, apiServerMockHandlerFilename, str(apiServerTcpPort)])
         time.sleep(0.2)
 
     def tearDown(self):
         # Stopping APIserver mock-object
         self.apiServerMockSubprocess.kill()
+        # TODO: Restoring DXPY env settings
 
-    def test_exponential_retry(self):
-        requests.post("http://127.0.0.1:8080/file/new", data = {'key':'value'})
+    def test_503_exponential_retry(self):
+        res = dxpy.DXHTTPRequest("/system/whoami", {}, want_full_response=True)
         apiServerStats = json.loads(requests.get("http://127.0.0.1:8080/stats").content)
         print("APIserver stats is '{}'".format(apiServerStats))
 
